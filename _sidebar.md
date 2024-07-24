084077d8583ae2e66bfdd04e46e4228f83e757af
xuxihai <xuxh@tuwan.com>
2024-07-24 09:34:20 +0800

    update

---
+++ _sidebar.md
@@ -0,0 +1,6 @@
+<!-- _sidebar.md -->
+
+- 前端
+  - [nginx 403错误](category/frontend/nginx-403.md)
+  - [获取当前脚本](category/frontend/get-current-script.md)
+
---
+++ config.js
@@ -0,0 +1,32 @@
+window.$docsify = {
+  loadSidebar: true,
+  subMaxLevel: 3,
+  maxLevel: 3,
+  auto2top: true,
+  name: 'haixuxu site',
+
+  copy: {
+    errorText: '复制出错',
+    successText: '已复制到剪贴板'
+  },
+
+  search: {
+    paths: 'auto',
+
+    placeholder: {
+      '/': '(o゜▽゜)o☆ 搜一下'
+    },
+
+    noData: {
+      '/': '找不到结果╮(╯▽╰)╭'
+    }
+  },
+
+  toc: {
+    title: 'haixuxu'
+  },
+  alias: {
+    '/.*/_sidebar.md': '/_sidebar.md'
+  },
+  // coverpage: 'README.md'
+};
